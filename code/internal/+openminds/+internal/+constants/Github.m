126387f65622774697cfd3965f5ef874aa99371d
ehennestad <ehennestad@gmail.com>
2023-08-22 17:24:47 +0200

    Add functions for retrieving controlled instances from OMI repository

---
+++ code/internal/+openminds/+internal/+constants/Github.m
@@ -0,0 +1,34 @@
+classdef Github < handle
+
+    properties (Constant)
+        BaseURL = "https://github.com"
+        RawContentUrl = "https://raw.githubusercontent.com"
+        Organization = "openMetadataInitiative"
+        Instances = "openMINDS_instances"
+    end
+
+    methods (Static)
+        
+        function url = getRepositoryUrl(repoName)
+            import openminds.internal.utility.string.uriJoin
+            import openminds.internal.constants.Github
+
+            switch repoName
+                case {'openMINDS_instances', 'instances'}
+                    url = uriJoin(Github.BaseURL, Github.Instances);
+            end
+        end
+
+        function url = getRawFileUrl(repoName, pathString)
+            import openminds.internal.utility.string.uriJoin
+            import openminds.internal.constants.Github
+
+            switch repoName
+                case {'openMINDS_instances', 'instances'}
+                    baseUrl = uriJoin(Github.RawContentUrl, Github.Organization, Github.Instances);
+            end
+
+            url = uriJoin(baseUrl, pathString);
+        end
+    end
+end
--- code/internal/+openminds/+internal/getControlledInstance.m
+++ code/internal/+openminds/+internal/getControlledInstance.m
@@ -5,6 +5,42 @@ function data = getControlledInstance(instanceName, schemaName, modelName)
         schemaName (1,1) string
         modelName (1,1) string
     end
+    
+    try
+        data = getOnlineInstance(instanceName, schemaName, modelName);
+    catch
+        data = getOfflineInstance(instanceName, schemaName, modelName);
+    end
+end
+
+function data = getOnlineInstance(instanceName, schemaName, modelName)
+       
+    arguments
+        instanceName (1,1) string
+        schemaName (1,1) string
+        modelName (1,1) string = "controlledTerms"
+    end
+        
+    import openminds.internal.constants.Github
+    import openminds.internal.utility.string.uriJoin
+    
+    if ~strcmp( upper(schemaName{1}(1:2)), schemaName{1}(1:2))
+        schemaName{1}(1) = lower(schemaName{1}(1));
+    end
+
+    % Note: modelName is currently not used.
+
+    % Build url for file
+    branchUrl = "main/instances/latest/terminologies";
+    instancePath = uriJoin(branchUrl, schemaName, instanceName+".jsonld");
+
+    url = Github.getRawFileUrl("instances", instancePath);
+
+    jsonStr = webread(url);
+    data = openminds.internal.utility.json.decode(jsonStr);
+end
+
+function data = getOfflineInstance(instanceName, schemaName, modelName)
 
     import openminds.internal.listControlledInstances
     
