34d62644c6ab90df15a1da6299755c71284da585
ehennestad <ehennestad@gmail.com>
2023-10-03 22:14:38 +0200

    Update mustBeValidEmail.m
    Fixed bug in email validator

--- code/internal/property_validation/mustBeValidEmail.m
+++ code/internal/property_validation/mustBeValidEmail.m
@@ -1,9 +1,11 @@
 function mustBeValidEmail(value)
     % todo: include dashes and dots, and multilevel domains. 
     % Compare match to value to see if they are equal..
-    matchedPattern = regexp(value, '\w*\@\w*\.\w*', 'match');
-    isValid = ~isempty(matchedPattern);
-    if ~isValid()
-        error('%s is not a valid email adress')
+    if ~isempty(value)
+        matchedPattern = regexp(value, '\w*\@\w*\.\w*', 'match');
+        isValid = ~isempty(matchedPattern);
+        if ~isValid()
+            error('"%s" is not a valid email adress', value)
+        end
     end
 end
