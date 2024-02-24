5812c3f97d04ec17b4b4d94975cf5cb2e63dc361
ehennestad <ehennestad@gmail.com>
2024-02-24 16:04:59 +0100

    Add validation functions for properties of date and time format

---
+++ code/internal/property_validation/mustBeValidTime.m
@@ -0,0 +1,6 @@
+function mustBeValidTime(value)
+    isValid = value.Year == 0 && value.Month == 0 && value.Day == 0;
+    if ~isValid
+        warning('Value must be a datetime value without date information')
+    end
+end
