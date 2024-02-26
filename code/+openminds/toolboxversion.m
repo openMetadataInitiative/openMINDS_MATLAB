bf134d318ed0aff8e99110ded85caaa5aff55b1f
ehennestad <ehennestad@gmail.com>
2024-02-26 13:33:50 +0100

    Update toolboxversion.m
    Fix function to also capture sub-patch release number

--- code/+openminds/toolboxversion.m
+++ code/+openminds/toolboxversion.m
@@ -1,9 +1,22 @@
 function versionStr = toolboxversion()
+% toolboxversion - Get the version identifier for the openMINDS toolbox
+
     rootPath = openminds.internal.rootpath();
     contentsFile = fullfile(rootPath, 'Contents.m');
 
     fileStr = fileread(contentsFile);
    
-    mathcedStr = regexp(fileStr, 'Version \d*\.\d*\.\d*', 'match');
-    versionStr = mathcedStr{1};
+    % First try to get a version with a sub-patch version number
+    matchedStr = regexp(fileStr, 'Version \d*\.\d*\.\d*.\d*(?= )', 'match');
+
+    % If not found, get major-minor-patch
+    if isempty(matchedStr)
+        matchedStr = regexp(fileStr, 'Version \d*\.\d*\.\d*(?= )', 'match');
+    end
+
+    if isempty(matchedStr)
+        error('openMINDS:Version:VersionNotFound', ...
+            'No version was detected for this openminds installation.')
+    end
+    versionStr = matchedStr{1};
 end
