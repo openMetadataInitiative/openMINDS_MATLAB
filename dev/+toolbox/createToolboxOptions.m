5abc75472158ed720606c265f68ae6768d539500
ehennestad <ehennestad@gmail.com>
2024-09-29 14:53:52 +0200

    Create createToolboxOptions.m

---
+++ dev/+toolbox/createToolboxOptions.m
@@ -0,0 +1,56 @@
+function toolboxOptions = createToolboxOptions(versionNumber)
+% Script for setting up toolbox (mltbx) options for openMINDS_MATLAB
+%
+%   Example:
+%       toolboxOptions = getToolboxOptions('0.9.2')
+
+    arguments
+        versionNumber (1,1) string {mustBeValidVersionNumber(versionNumber)}
+    end
+
+    MLTBX_NAME = "openMINDS_MATLAB";
+    
+    projectRootDir = ommtools.getProjectRootDir();
+    toolboxInfoFilePath = fullfile(projectRootDir, "dev", "toolbox_info.json");
+    toolboxInfo = jsondecode(fileread(toolboxInfoFilePath));
+
+    % Get toolbox identifier
+    identifier = toolboxInfo.Identifier; 
+    toolboxInfo = rmfield(toolboxInfo, 'Identifier');
+
+    toolboxFolder = fullfile(projectRootDir, "code");
+    opts = matlab.addons.toolbox.ToolboxOptions(toolboxFolder, identifier, toolboxInfo);
+    
+    opts.ToolboxVersion = versionNumber;
+
+    toIgnore = contains(opts.ToolboxFiles, '_dev');
+    opts.ToolboxFiles = opts.ToolboxFiles(~toIgnore);
+
+    opts.ToolboxImageFile = fullfile(projectRootDir, "img", "light_openMINDS-MATLAB-logo_toolbox.png");
+    opts.ToolboxGettingStartedGuide = fullfile(projectRootDir, "code", "gettingStarted.mlx");
+
+    % Specify toolbox path TODO: empty folders
+    toolboxPathFolders = [...
+        fullfile(projectRootDir, "code"), ...
+        fullfile(projectRootDir, "code", "internal"), ...
+        fullfile(projectRootDir, "code", "internal", "property_validation"), ...
+        fullfile(projectRootDir, "code", "livescripts"), ...
+        fullfile(projectRootDir, "code", "schemas", "latest"), ...
+        fullfile(projectRootDir, "code", "mixedtypes", "latest"), ...
+        fullfile(projectRootDir, "code", "enumerations", "latest") ...
+    ];
+    opts.ToolboxMatlabPath = toolboxPathFolders;
+    
+    opts.SupportedPlatforms.Win64 = true;
+    opts.SupportedPlatforms.Maci64 = true;
+    opts.SupportedPlatforms.Glnxa64 = true;
+    opts.SupportedPlatforms.MatlabOnline = true;
+    
+    % Todo: populate required addons from requirements file
+
+    versionNumber = strrep(opts.ToolboxVersion, '.', '_');
+    outputFileName = sprintf('%s_v%s.mltbx', MLTBX_NAME, versionNumber);
+    opts.OutputFile = fullfile(projectRootDir, "releases", outputFileName);
+
+    toolboxOptions = opts;
+end
