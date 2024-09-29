ee07559164c5fde2c395d6f5981373c2c2a82ee1
MATLAB Release by ehennestad <>
2024-09-29 19:04:58 +0000

    Final checkins for release v0.9.3

--- code/Contents.m
+++ code/Contents.m
@@ -1,5 +1,5 @@
 % openMINDS Metadata Models
-% Version 0.9.2 (R2022b) 04-09-2024
+% Version 0.9.3 (R2021b+) 29-Sep-2024
 %
 % Copyright (c) 2024, openMetadataInitiative
 % ------------------------------------------
