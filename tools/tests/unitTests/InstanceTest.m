1b18f0b41bd4e00ae61e4508fc7a1d2e5f7c8b9b
ehennestad <ehennestad@gmail.com>
2024-10-30 18:47:02 +0100

    Rename test to InstanceTest

--- tools/tests/unitTests/InstanceTest.m
+++ tools/tests/unitTests/InstanceTest.m
@@ -1,5 +1,5 @@
-classdef testInstanceSaveLoad < matlab.unittest.TestCase
-% testInstanceSaveLoad Unit test for save and load operations on OpenMINDS types
+classdef InstanceTest < matlab.unittest.TestCase
+% InstanceTest - Unit test for creation and save/load of OpenMINDS instances
 
     % Automatically generate a test case for each schema type
     properties (TestParameter)
