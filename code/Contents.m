3796a6b0c970375a76110f4a7a47d4c92197055a
ehennestad <ehennestad@gmail.com>
2024-11-15 12:55:36 +0100

    Update Contents.m

--- code/Contents.m
+++ code/Contents.m
@@ -1,5 +1,5 @@
 % openMINDS Metadata Models
-% Version 0.9.4 (R2022b+) 15-Nov-2024
+% Version 0.9.3 (R2022b+) 15-Nov-2024
 %
 % Copyright (c) 2024, openMetadataInitiative
 % ------------------------------------------
