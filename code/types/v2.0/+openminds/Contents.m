cace3c338b526b2d2338f1f0eb0cf91e15e053eb
ehennestad <ehennestad@gmail.com>
2025-04-03 21:39:59 +0200

    Rename folder schemas to type

--- code/types/v2.0/+openminds/Contents.m
+++ code/types/v2.0/+openminds/Contents.m
@@ -1,5 +1,5 @@
 % openMINDS (model schemas - v2.0)
-%   The schema folder contains MATLAB schemas for the following openMINDS models:
+%   The schema folder contains MATLAB schemas for the following openMINDS modules:
 %
 %     controlledterms - openminds.controlledterms is a model.
 %                core - openminds.core is a model.
