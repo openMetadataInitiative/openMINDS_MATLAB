e96d1c6106d8b85852530689e95bacc20e647f02
ehennestad <ehennestad@gmail.com>
2025-04-03 21:48:42 +0200

    Move resource files from schemas to types folder

---
+++ code/internal/resources/content_files/types/v3.0/+openminds/Contents.m
@@ -0,0 +1,13 @@
+% openMINDS metadata types - v3.0
+%   The types folder contains MATLAB classes for metadata types of the 
+%   following openMINDS modules:
+%
+%           chemicals - openminds.chemicals is a module.
+%         computation - openminds.computation is a module.
+%     controlledterms - openminds.controlledterms is a module.
+%                core - openminds.core is a module.
+%               ephys - openminds.ephys is a module.
+%        publications - openminds.publications is a module.
+%               sands - openminds.sands is a module.
+%        specimenprep - openminds.specimenprep is a module.
+%         stimulation - openminds.stimulation is a module.
