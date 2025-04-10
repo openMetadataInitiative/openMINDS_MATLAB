f8d20a0899f6e12c91e7a350085fb7bb6fc95991
ehennestad <ehennestad@gmail.com>
2025-04-10 13:15:45 +0200

    Update instanceFromIRI.m
    Add input validation

--- code/+openminds/instanceFromIRI.m
+++ code/+openminds/instanceFromIRI.m
@@ -1,4 +1,14 @@
 function instance = instanceFromIRI(iri)
+    
+    validBaseIri = [...
+        "https://openminds.ebrains.eu", ...
+        "https://openminds.om-i.org",
+        ];
+
+    assert(startsWith(iri, validBaseIri), ...
+        'Expected IRI to start with %s\n', ...
+        strjoin("""" + validBaseIri + """", ' or '));
+
     [type, ~] = openminds.utility.parseAtID(iri);
     typeEnum = openminds.enum.Types(type);
     instance = feval(typeEnum.ClassName, iri);
