70b6839d550ab8a6affe1af43bbbb9dc3a447095
ehennestad <ehennestad@gmail.com>
2025-04-12 21:26:28 +0200

    Fix bugs and add tests for property validators (#56)
    * Delete mustBeEmptyOrGreaterThanOrEqual.m
    
    Apparently unused
    
    * Add tests fro mustBeListOfUniqueItems
    
    * Add test for mustBeSpecifiedLength and mustBeOneOf
    
    * Add tests for mustBeValidEmail and mustBeValidStringLength
    
    * Add error ids for property validators
    
    * Update code issues and tests badges
    
    * Add test for mustBeValidTime
    
    * Update code issues and tests badges
    
    * Update email validator and ad docstrings
    
    * Update mustBeListOfUnique items
    
    compare instances on identifiers instead of object equality
    
    * Update code issues and tests badges
    
    * Update InstanceTest.m
    
    Update terms, refer to metadata types instead of schemas
    
    * Fix bug if input to mustBeListOfUniqueItems is empty
    
    * Update code issues and tests badges
    
    ---------
    
    Co-authored-by: Run tests by ehennestad <>

--- code/internal/property_validation/mustBeValidTime.m
+++ code/internal/property_validation/mustBeValidTime.m
@@ -1,6 +1,11 @@
 function mustBeValidTime(value)
-    isValid = all(value.Year == 0 & value.Month == 0 & value.Day == 0);
+% mustBeValidTime - Check that datetime value only contains time values
+
+    [h, m, s] = hms(value); hmsArray = [h; m; s];
+    isValid = all(value.Year == -1) && all( sum(hmsArray, 1) ~= 0);
+
     if ~isValid
-        warning('Value must be a datetime value without date information')
+        warning('OPENMINDS_MATLAB:PropertyValidators:InvalidTime', ...
+            'Value must be a datetime value without date information')
     end
 end
---
+++ tools/tests/unitTests/PropertyValidatorTest.m
@@ -0,0 +1,157 @@
+classdef PropertyValidatorTest < matlab.unittest.TestCase
+    % PropertyValidatorTest - Unit tests for property validator functions
+    
+    methods (Test)
+        function testMustBeListOfUniqueItemsForMixedTypes(testCase)
+            import openminds.internal.mixedtype.organization.DigitalIdentifier
+            org = organizationWithTwoIds(); % From oneOffs
+            
+            % Should pass for property value of unique mixed types
+            mustBeListOfUniqueItems(org.digitalIdentifier)
+
+            nonUniqueDigitalIdentifiers = [...
+                org.digitalIdentifier, org.digitalIdentifier(1)];
+            
+            testCase.verifyError(...
+                @() mustBeListOfUniqueItems(nonUniqueDigitalIdentifiers), ...
+                'OPENMINDS_MATLAB:PropertyValidator:InstancesMustBeUnique')
+
+            % Simulate unresolved mixed type references.
+            digitalIdentifierA = DigitalIdentifier(struct('at_id', '_:000001'));
+            digitalIdentifierB = DigitalIdentifier(struct('at_id', '_:000002'));
+            
+            uniqueDigitalIdentifiers = [digitalIdentifierA, digitalIdentifierB];
+
+            % Should pass for different ids
+            mustBeListOfUniqueItems(uniqueDigitalIdentifiers)
+            
+            % Verify error for non-unique ids
+            digitalIdentifierC = DigitalIdentifier(struct('at_id', '_:000001'));
+            nonUniqueDigitalIdentifiers = [uniqueDigitalIdentifiers, digitalIdentifierC];
+            testCase.verifyError(...
+                @()  mustBeListOfUniqueItems(nonUniqueDigitalIdentifiers), ...
+                'OPENMINDS_MATLAB:PropertyValidator:InstancesMustBeUnique')
+        end
+        
+        function testMustBeListOfUniqueItemsForInstances(testCase)
+            subjectA = openminds.core.Subject();
+            subjectB = openminds.core.Subject();
+
+            % Should pass for unique items
+            mustBeListOfUniqueItems([subjectA, subjectB])
+
+            % Should fail for non-unique items
+            testCase.verifyError(...
+                @() mustBeListOfUniqueItems([subjectA, subjectA]), ...
+                'OPENMINDS_MATLAB:PropertyValidator:InstancesMustBeUnique')
+        end
+
+        function testMustBeListOfUniqueItemsForStrings(testCase)
+
+            % Should pass for list of unique string
+            mustBeListOfUniqueItems({'a', 'b'})
+
+            mustBeListOfUniqueItems(["a", "b"])
+
+            testCase.verifyError(...
+                @() mustBeListOfUniqueItems({'a', 'a'}), ...
+                'OPENMINDS_MATLAB:PropertyValidator:ValuesMustBeUnique')
+
+            testCase.verifyError(...
+                @() mustBeListOfUniqueItems(["a", "a"]), ...
+                'OPENMINDS_MATLAB:PropertyValidator:ValuesMustBeUnique')
+
+            testCase.verifyError(...
+                @() mustBeListOfUniqueItems(["a", "b", "a"]), ...
+                'OPENMINDS_MATLAB:PropertyValidator:ValuesMustBeUnique')
+        end
+
+        function testMustBeOneOf(testCase)
+            % Verify that validator works for the different alias names
+            mustBeOneOf(openminds.core.Person(), 'openminds.Person')
+            mustBeOneOf(openminds.core.Person(), 'openminds.core.Person')
+            mustBeOneOf(openminds.core.Person(), 'openminds.core.actors.Person')
+
+            % Verify that validator works for lists of types 
+            mustBeOneOf(...
+                {openminds.core.Person(), openminds.core.Organization}, ...
+                {'openminds.Person', 'openminds.Organization'})
+            
+            % Verify that validator fails is mismatch of given types and allowed types 
+            testCase.verifyError( ...
+                @() mustBeOneOf({openminds.core.Person(), openminds.core.Organization}, {'openminds.Person'}), ...
+                'OPENMINDS_MATLAB:PropertyValidators:MustBeTypeOf')
+        end
+
+        function testMustBeSpecifiedLength(testCase)
+            testValue = 1:5;
+            
+            % Test function when value exactly matches the required length
+            mustBeSpecifiedLength(testValue, 1, 5)
+            
+            % Test function when value is inside the required length
+            % interval
+            mustBeSpecifiedLength(testValue, 0, 6)
+
+            % Test function when value is too long
+            testCase.verifyError(...
+                @() mustBeSpecifiedLength(testValue, 1, 4), ...
+                'OPENMINDS_MATLAB:PropertyValidators:ListIsTooLong')
+
+            % Test function when value is too long
+            testCase.verifyError(...
+                @() mustBeSpecifiedLength(testValue, 10, 15), ...
+                'OPENMINDS_MATLAB:PropertyValidators:ListIsTooShort')
+        end
+
+        function testMustBeValidEmail(testCase)
+            mustBeValidEmail('eivihe@uio.no')
+
+            testCase.verifyError(...
+                @() mustBeValidEmail('eivihe@uio'), ...
+                'OPENMINDS_MATLAB:PropertyValidators:InvalidEmail')
+        end
+
+        function testMustBeValidStringLength(testCase)
+            testValue = "hello";
+            
+            % Test function when value exactly matches the required length
+            mustBeValidStringLength(testValue, 1, 5)
+            
+            % Test function when value is inside the required length
+            % interval
+            mustBeValidStringLength(testValue, 0, 6)
+
+            % Test function when value is too long
+            testCase.verifyError(...
+                @() mustBeValidStringLength(testValue, 1, 4), ...
+                'OPENMINDS_MATLAB:PropertyValidators:InvalidStringLength')
+
+            % Test function when value is too long
+            testCase.verifyError(...
+                @() mustBeValidStringLength(testValue, 10, 15), ...
+                'OPENMINDS_MATLAB:PropertyValidators:InvalidStringLength')
+        end
+
+        function testMustBeValidTime(testCase)
+
+            validTime = datetime(0,0,0,18,30,10);
+            inValidTime =  datetime(2024,1,1,18,30,10);
+            inValidTimeNoTime =  datetime(2024,1,1,0,0,0);
+
+
+            % Test function when value is a valid time
+            mustBeValidTime(validTime)
+
+            % Test function when datetime contains a year
+            testCase.verifyWarning(...
+                @() mustBeValidTime(inValidTime), ...
+                'OPENMINDS_MATLAB:PropertyValidators:InvalidTime')
+
+            % Test function when there is no time info
+            testCase.verifyWarning(...
+                @() mustBeValidTime(inValidTimeNoTime), ...
+                'OPENMINDS_MATLAB:PropertyValidators:InvalidTime')
+        end
+    end
+end
