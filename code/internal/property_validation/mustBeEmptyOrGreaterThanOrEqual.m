e224a2489ed28748966bb023b1e65c7c8305aa02
ehennestad <ehennestad@gmail.com>
2025-04-12 10:07:13 +0200

    Delete mustBeEmptyOrGreaterThanOrEqual.m
    Apparently unused

--- code/internal/property_validation/mustBeEmptyOrGreaterThanOrEqual.m
+++
@@ -1,8 +0,0 @@
-function mustBeEmptyOrGreaterThanOrEqual(value, minimumValue)
-%mustBeEmptyOrGreaterThanOrEqual
-
-    isValid = isnan(value) || value > minimumValue;
-
-    msg = sprintf('Value must be greater than or equal to %d.', minimumValue);
-    assert( isValid, msg)
-end
