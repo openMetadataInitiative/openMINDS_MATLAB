f3f4b5b16abce81888ff21b08bac03ddf42bd1b6
Create new release by ehennestad <ehennestad@gmail.com>
2025-07-25 19:30:50 +0000

    Final check-ins for release v0.9.6 [skip-ci]

--- code/Contents.m
+++ code/Contents.m
@@ -1,5 +1,5 @@
 % openMINDS Metadata Toolbox
-% Version 0.9.5 (R2021b+) 14-Apr-2025
+% Version 0.9.6 (R2021b+) 25-Jul-2025
 %
 % Copyright (c) 2025, openMetadataInitiative
 % ------------------------------------------
