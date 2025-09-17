619a6720215bf175644d437f04fa56befbfa9f2c
ehennestad <ehennestad@gmail.com>
2025-09-17 14:29:12 +0200

    Update mustBeValidDate.m
    - Skip assertion if value is NaT (not a time)

--- code/internal/property_validation/mustBeValidDate.m
+++ code/internal/property_validation/mustBeValidDate.m
@@ -1,4 +1,8 @@
 function mustBeValidDate(value)
+    % Todo: Should this be considered invalid?
+    % Skip NaT - Result of adding empty string to property value
+    if isnat(value); return; end
+
     assert( all(value.Hour == 0 & value.Minute == 0 & value.Second == 0), ...
         'OPENMINDS_MATLAB:PropertyValidators:InvalidDate', ...
         'Value must be a datetime value without time information')
