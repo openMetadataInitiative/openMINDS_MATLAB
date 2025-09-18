c3b499ade67d4a3d1ec58fc0ab01c0bc5dc82027
ehennestad <ehennestad@gmail.com>
2025-09-18 20:10:44 +0200

    Renamed "openminds.utility.isSemanticSchemaName" to "openminds.utility.isTypeIRI".

--- code/internal/+openminds/+utility/isSemanticSchemaName.m
+++
@@ -1,4 +0,0 @@
-function tf = isSemanticSchemaName(fullSchemaName)
-%isSemanticSchemaName Check if name is a semantic name
-    tf = startsWith(fullSchemaName, openminds.constant.BaseURI);
-end
---
+++ code/internal/+openminds/+utility/isTypeIRI.m
@@ -0,0 +1,15 @@
+function tf = isTypeIRI(name)
+%isTypeIRI Check if name is a semantic name of a schema (type IRI)
+    
+    tf = false;
+    disp(rand())
+    if startsWith(name, openminds.constant.BaseURI('v1')) % v1-v3
+        [~, modules] = enumeration('openminds.enum.Modules');
+        typeBaseIRI = openminds.constant.BaseURI('v1') + "/" + modules;
+        tf = any(startsWith(name, typeBaseIRI));
+    
+    elseif startsWith(name, openminds.constant.BaseURI('v4')) %v4+
+        typeBaseIRI = openminds.constant.BaseURI + "/types";
+        tf = startsWith(name, typeBaseIRI);
+    end
+end
