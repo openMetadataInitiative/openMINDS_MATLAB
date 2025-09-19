051851fc67e03b0cb717f160714fbb0f12f7a40d
ehennestad <ehennestad@gmail.com>
2025-09-19 09:39:19 +0200

    Update CustomInstanceDisplay.m
    - Changed header annotation: show id for scalar objects
    - Added method for displaying all properties, or only non-empty properties
    - Annotation for scalar mixed type object in single-line view is the type of the instance, not all allowed types

--- code/internal/+openminds/+internal/+mixin/CustomInstanceDisplay.m
+++ code/internal/+openminds/+internal/+mixin/CustomInstanceDisplay.m
@@ -5,7 +5,7 @@ classdef CustomInstanceDisplay < handle & matlab.mixin.CustomDisplay & ...
 %
 %   Known subclasses:
 %       - openminds.abstract.Schema
-%       - openmidns.internal.abstract.MixedTypeSet
+%       - openminds.internal.abstract.MixedTypeSet
 
 % Todo:
 %  [ ] Clean up and improve Compact Displays
@@ -14,15 +14,17 @@ classdef CustomInstanceDisplay < handle & matlab.mixin.CustomDisplay & ...
 %  [ ] Distinguish between arrays and scalars...
 %  [ ] getFooter is different from instance/schema and mixedInstance...
 
-    % Dependencies that should be handled better:
-    %  - X_TYPE
     
     properties (Dependent, Transient, Hidden)
         DisplayString
     end
+
+    properties (Constant, Access = protected)
+        REFERENCE_DISPLAY_LABEL = "<reference>"
+        NULLVALUE_DISPLAY_LABEL = 'None'; % 'Not set', 'Not present', 'N/A';
+    end
     
     methods
-
         function obj = CustomInstanceDisplay()
             %pass
         end
@@ -43,20 +45,39 @@ classdef CustomInstanceDisplay < handle & matlab.mixin.CustomDisplay & ...
             displayLabel = obj.getDisplayLabel();
             if isempty(displayLabel)
                 displayLabel = '<not named>';
+            if obj.isReference()
+                displayLabel = obj.REFERENCE_DISPLAY_LABEL;
+            else
+                displayLabel = obj.getDisplayLabel();
+                if isempty(displayLabel)
+                    displayLabel = '<not named>';
+                end
             end
         end
     end
 
     methods (Abstract, Access = protected) % Subclasses must implement.
         
+    % Abstract - Subclasses must implement:
+    methods (Abstract, Access = protected) 
+        tf = isReference(obj)
+
         str = getDisplayLabel(obj)
         
         annotation = getAnnotation(obj)
+        annotation = getAnnotation(obj, width)
 
         % str = getInstanceType(obj)
     end
 
     methods (Hidden, Access = protected) % CustomDisplay - Method implementation
+    % Abstract - Subclasses must implement:
+    methods (Abstract, Access = ?openminds.internal.mixin.CustomInstanceDisplay)
+        semanticName = getSemanticName(obj)
+    end
+
+    % CustomDisplay - Method implementations
+    methods (Hidden, Access = protected)
         
         function requiredProperties = getRequiredProperties(~)
             % Subclasses should override.
@@ -72,11 +93,15 @@ classdef CustomInstanceDisplay < handle & matlab.mixin.CustomDisplay & ...
             
             if numel(obj) == 0
                 docLinkStr = sprintf('Empty %s', docLinkStr);
+                annotation = semanticName;
+            elseif isscalar(obj)
+                annotation = obj.id;
             elseif numel(obj) > 1
                 docLinkStr = sprintf('1x%d %s', numel(obj), docLinkStr);
+                annotation = semanticName;
             end
 
-            str = sprintf('  %s (%s) with properties:\n', docLinkStr, semanticName);
+            str = sprintf('  %s (%s) with properties:\n', docLinkStr, annotation);
         end
 
         function str = getFooter(obj)
@@ -95,13 +120,52 @@ classdef CustomInstanceDisplay < handle & matlab.mixin.CustomDisplay & ...
                 str = sprintf('  %s\n', str);
             end
         end
+
+        function groups = getPropertyGroups(obj)
+
+            displayPref = getpref('openminds', 'display', 'all');
+            if strcmp(displayPref, 'all')
+                groups = getPropertyGroups@matlab.mixin.CustomDisplay(obj);
+            else
+                propNames = properties(obj);
+                s = struct();
+                for i = 1:numel(propNames)
+                    if ~isempty( obj.(propNames{i}) )
+                        s.(propNames{i}) = obj.(propNames{i});
+                    end
+                end
+    
+                groups = matlab.mixin.util.PropertyGroup(s);
+            end
+        end
         
         function displayNonScalarObject(obj)
             displayNonScalarObject@matlab.mixin.CustomDisplay(obj)
+        % displayNonScalarObject - Override non-scalar display
+
+            % Get single line representation for each element
+            repArray = arrayfun(@(o) o.compactRepresentationForSingleLine, obj, 'UniformOutput', false);
+            stringArray = cellfun(@(r) "    "+ r.PaddedDisplayOutput, repArray);
+            
+            % Remove array brackets          
+            stringArray = strrep(stringArray, '[', '');
+            stringArray = strrep(stringArray, ']', '');
+
+            % Display
+            headerStr = obj.getHeader();
+            headerStr = strrep(headerStr, 'with properties', 'with elements');
+            disp(headerStr)
+            if iscell(stringArray) || isstring(stringArray)
+                fprintf( '%s\n\n', strjoin(stringArray, newline) );
+            else
+                fprintf( '%s\n\n', stringArray)
+            end
         end
     end
 
     methods (Hidden) % % CustomCompactDisplayProvider - Method implementation
+    % CustomCompactDisplayProvider - Method implementation
+    methods (Hidden)
         
         function rep = compactRepresentationForSingleLine(obj, displayConfiguration, width)
             import openminds.internal.utility.getSchemaName
@@ -110,6 +174,10 @@ classdef CustomInstanceDisplay < handle & matlab.mixin.CustomDisplay & ...
                 displayConfiguration = matlab.display.DisplayConfiguration();
             end
 
+            if nargin < 3
+                width = inf;
+            end
+            
             numObjects = numel(obj);
 
             annotation = obj.getAnnotation();
@@ -120,14 +188,35 @@ classdef CustomInstanceDisplay < handle & matlab.mixin.CustomDisplay & ...
                 % str = sprintf("0x0 empty %s", schemaName);
                 rep = matlab.display.PlainTextRepresentation(obj, str, displayConfiguration, 'Annotation', annotation);
                 % rep = fullDataRepresentation(obj, displayConfiguration, 'StringArray', string(str));
+                str = openminds.internal.mixin.CustomInstanceDisplay.NULLVALUE_DISPLAY_LABEL;
+                annotation = obj.getAnnotation(width);
+                % displayConfiguration.DataDelimiters=["<", ">"];
+
+                rep = matlab.display.PlainTextRepresentation(...
+                    obj, str, displayConfiguration, 'Annotation', annotation);
 
             elseif numObjects == 1
                 rep = fullDataRepresentation(obj, displayConfiguration, 'StringArray', obj.DisplayString, 'Annotation', annotation);
             
+                % Create a representation without annotation to learn it's width
+                rep = fullDataRepresentation(obj, displayConfiguration, ...
+                    'StringArray', obj.DisplayString);
+                
+                widthForAnnotation = width - rep.CharacterWidth - 3; % 3 = annotation padding " ()" 
+                annotation = obj.getAnnotation(widthForAnnotation);
+                
+                rep = fullDataRepresentation(obj, displayConfiguration, ...
+                    'StringArray', obj.DisplayString, ...
+                    'Annotation', annotation);
             else
                 stringArray = obj.getStringArrayForSingleLine();
+                stringArray = obj.getStringArrayForSingleLine(displayConfiguration, width);
+                annotation = obj.getAnnotation(width);
 
                 rep = fullDataRepresentation(obj, displayConfiguration, 'StringArray', stringArray, 'Annotation', annotation);
+                rep = fullDataRepresentation(obj, displayConfiguration, ...
+                    'StringArray', stringArray, ...
+                    'Annotation', annotation);
                 
                 count = 1;
 
@@ -136,6 +225,8 @@ classdef CustomInstanceDisplay < handle & matlab.mixin.CustomDisplay & ...
                 sizeTypeStr = strrep(sizeTypeStr, '[', '(');
                 sizeTypeStr = strrep(sizeTypeStr, ']', ')');
 
+                % Iterate to find a string that fits within the display
+                count = 1;
                 while rep.CharacterWidth > width
                     tempStringArray = stringArray(1:end-count);
                     if isempty(tempStringArray)
@@ -144,6 +235,9 @@ classdef CustomInstanceDisplay < handle & matlab.mixin.CustomDisplay & ...
 
                     tempStringArray(end) = tempStringArray(end) + "... " + sizeTypeStr;
                     rep = fullDataRepresentation(obj, displayConfiguration, 'StringArray', tempStringArray, 'Annotation', annotation);
+                    rep = fullDataRepresentation(obj, displayConfiguration, ...
+                        'StringArray', tempStringArray, ...
+                        'Annotation', annotation);
                     count = count + 1;
                 end
             end
@@ -191,34 +285,15 @@ classdef CustomInstanceDisplay < handle & matlab.mixin.CustomDisplay & ...
     methods (Access = protected)
         
         function stringArray = getStringArrayForSingleLine(obj)
+        function stringArray = getStringArrayForSingleLine(obj, displayConfiguration, width) %#ok<INUSD> Subclasses might use extra input args
             stringArray = arrayfun(@(o) o.DisplayString, obj, 'UniformOutput', false);
             if iscell(stringArray)
                 stringArray = string(stringArray);
             else
                 warning('A cell array was expected, but result was %s. Please report.', class(stringArray))
             end
-            % Note: 2023-11-03: This was returning a cell array but should
-            % return a string array...
-        end
-    end
-
-    methods (Access = protected)
-
-        function semanticName = getSemanticName(obj)
-
-            import openminds.internal.utility.string.packageParts
-            import openminds.internal.utility.string.camelCase
-
-            fullClassName = class(obj);
-
-            if contains(fullClassName, 'mixedtype')
-                BASE_URL = "https://openminds.ebrains.eu/vocab/";
-                [~, shortClassName] = packageParts(fullClassName);
-                shortClassName = camelCase(shortClassName);
-                semanticName = BASE_URL + shortClassName;
-            else
-                semanticName = eval( [fullClassName '.X_TYPE'] );
-            end
+            % Note: 2023-11-03: This function was returning a cell array but 
+            % should return a string array...
         end
     end
 end
